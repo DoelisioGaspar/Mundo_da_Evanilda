73e7f3f8305a00c01e4241611df825343f6ce0fd
DoelisioGaspar <doegaspar123@gmail.com>
2026-05-22 19:42:17 +0100

    commit--30

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
-import { produto } from './produtos.js'; 
+import { produto } from "./produtos.js";
 
-const areaProduto = document.querySelector('.area__produtos')
+const areaProduto = document.querySelector(".area__produtos");
 
 const renderProdutos = (lista) => {
-    areaProduto.innerHTML = lista.map(p => `
-        <div class="produto slide-in" data-id="${p.id}" data-name="${p.nome.split(' ').join('-')}">
+  areaProduto.innerHTML = lista
+    .map(
+      (p) => `
+        <div class="produto slide-in" data-id="${p.id}" data-name="${p.nome.split(" ").join("-")}">
             
             <div class="produto__imagem">
                 <img src="${p.imagem}" alt="${p.nome}">
@@ -28,48 +30,46 @@ const renderProdutos = (lista) => {
             </div>
             
         </div>
-    `).join('');
+    `,
+    )
+    .join("");
 
-    const caixaCurti = document.querySelectorAll('.lovebox');
-    caixaCurti.forEach(function(box) {
-    box.addEventListener('click', function() {
-        const likeIcon = this.querySelector('.like');
-        likeIcon.classList.toggle('liked');
+  const caixaCurti = document.querySelectorAll(".lovebox");
+  caixaCurti.forEach(function (box) {
+    box.addEventListener("click", function () {
+      const likeIcon = this.querySelector(".like");
+      likeIcon.classList.toggle("liked");
     });
-});
+  });
 };
 
-produto.then(data => {
-    renderProdutos(data);
+produto.then((data) => {
+  renderProdutos(data);
 });
 
-
-const hideButton = document.querySelectorAll('#hideButton');
+const hideButton = document.querySelectorAll("#hideButton");
 hideButton.forEach((bt, indice) => {
-    bt.addEventListener('click', () => {
-        let hideInputsRadio = document.querySelectorAll('.select-input')[indice]
-        hideInputsRadio.classList.toggle('hide')
-        
-        if (bt.classList == 'fa fa-minus') {
-           bt.classList.remove('fa-minus')
-           bt.classList.add('fa-plus') 
-        } else {
-            bt.classList.remove('fa-plus')
-            bt.classList.add('fa-minus')
-        }
-    })
-})
+  bt.addEventListener("click", () => {
+    let hideInputsRadio = document.querySelectorAll(".select-input")[indice];
+    hideInputsRadio.classList.toggle("hide");
 
+    if (bt.classList == "fa fa-minus") {
+      bt.classList.remove("fa-minus");
+      bt.classList.add("fa-plus");
+    } else {
+      bt.classList.remove("fa-plus");
+      bt.classList.add("fa-minus");
+    }
+  });
+});
 
 /* Filtrar produtos por categoria */
 
 function filtrarProdutos(data, categoria) {
-
-
-    areaProduto.innerHTML = '';
-    for(let i = 0; i < data.length; i++) {
-        if(data[i].categoria === categoria || data[i].keyword === categoria) {
-            areaProduto.innerHTML += `
+  areaProduto.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].categoria === categoria || data[i].keyword === categoria) {
+      areaProduto.innerHTML += `
             <div class="produto slide-in" data-id="${data[i].id}" data-name="${data[i].nome}">
             
                 <div class="produto__imagem">
@@ -83,16 +83,16 @@ function filtrarProdutos(data, categoria) {
                     </div>
                 
                     <div class="lovebox">
-                        <span class="fas fa-heart like ${data[i].liked ? 'liked' : ''}"></span>
+                        <span class="fas fa-heart like ${data[i].liked ? "liked" : ""}"></span>
                     </div>
-            
                 </div>
 
-            </div>
-            `
-        } else {
-            // Tela de produto não encontrado (UI page not found)
-            areaProduto.innerHTML = `
+            </div>`;
+      return;
+    }
+  }
+  // Tela de produto não encontrado (UI page not found)
+  areaProduto.innerHTML = `
             <div class="item--not--found slide-in">
                 <div class="produto__imagem" style="background: transparent;">
                     <img src="assets/images/undraw_empty_4zx0.svg" alt="Produto não encontrado">
@@ -103,58 +103,55 @@ function filtrarProdutos(data, categoria) {
                     <p>Desculpe, o produto que você está procurando não está disponível.</p>
                 </div>
                 <button>Pesquisar novamente</button>
-            </div>
-            `
-        }
+            </div>`;
 
-    }
-
-    const caixaCurti = document.querySelectorAll('.lovebox');
-    caixaCurti.forEach(function(box) {
-    box.addEventListener('click', function() {
-        const likeIcon = this.querySelector('.like');
-        likeIcon.classList.toggle('liked');
+  const caixaCurti = document.querySelectorAll(".lovebox");
+  caixaCurti.forEach(function (box) {
+    box.addEventListener("click", function () {
+      const likeIcon = this.querySelector(".like");
+      likeIcon.classList.toggle("liked");
     });
-});
+  });
 }
 
-
-const radio = document.querySelectorAll('#categoriaa');
-radio.forEach(r =>  {
-    r.addEventListener('click', () => {
-        if (r.value == "Todos") {
-            produto.then(data => {
-                renderProdutos(data);
-            });
-        }
-        else {
-            //Renderizar apenas os produtos com a categoria selecionada
-            produto.then(data => {
-                filtrarProdutos(data, r.value);
-            });
-        }
-    })
-}) 
-
-areaProduto.addEventListener('click', (event) => {
-    const produtoElement = event.target.closest('.produto');
-    if (produtoElement) {
-        // Handle product click event
-        window.location.href = `/produto?name=${produtoElement.dataset.name}`;
+const radio = document.querySelectorAll("#categoriaa");
+radio.forEach((r) => {
+  r.addEventListener("click", () => {
+    if (r.value == "Todos") {
+      produto.then((data) => {
+        renderProdutos(data);
+      });
+    } else {
+      //Renderizar apenas os produtos com a categoria selecionada
+      produto.then((data) => {
+        filtrarProdutos(data, r.value);
+      });
     }
+  });
 });
 
-document.addEventListener('click', (event) => {
-    if (event.target.id === 'filterButton') {
-        const filterButton = document.getElementById('filterButton');
-        const sideBar = document.querySelector('.sideBar');
-        sideBar.style.left= sideBar.style.left === '0px' ? '-100%' : '0px';
-        console.log('clicou');
-        //Se o evento for fora, fecha a barra lateral
-        document.addEventListener('click', (event) => {
-            if (!sideBar.contains(event.target) && event.target.id !== 'filterButton') {
-                sideBar.style.left = '-100%';
-            }
-        });
-    } 
+areaProduto.addEventListener("click", (event) => {
+  const produtoElement = event.target.closest(".produto");
+  if (produtoElement) {
+    // Handle product click event
+    window.location.href = `/produto?name=${produtoElement.dataset.name}`;
+  }
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target.id === "filterButton") {
+    const filterButton = document.getElementById("filterButton");
+    const sideBar = document.querySelector(".sideBar");
+    sideBar.style.left = sideBar.style.left === "0px" ? "-100%" : "0px";
+    console.log("clicou");
+    //Se o evento for fora, fecha a barra lateral
+    document.addEventListener("click", (event) => {
+      if (
+        !sideBar.contains(event.target) &&
+        event.target.id !== "filterButton"
+      ) {
+        sideBar.style.left = "-100%";
+      }
+    });
+  }
 });
