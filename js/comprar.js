0c7336025cd43c44033b1af733f91ca2abe8c2ac
DoelisioGaspar <doegaspar123@gmail.com>
2026-06-01 14:55:14 +0100

    c3

--- js/comprar.js
+++ js/comprar.js
@@ -1,6 +1,5 @@
 /* Puxar um único produto específico para compra*/
 import { produto } from './produtos.js';
-import { renderProdutos } from './app.js';
 
 export default function renderProductSelected(data) {
     const urlParams = new URLSearchParams(window.location.search);
@@ -48,11 +47,41 @@ produto.then(data => {
     console.error('Error rendering product:', error);
 });
 
+const containerMoreProductsLike = document.queryselector('.container-more-products-like')
 
-const containerMoreProductsLike = document.querySelector('.container-more-products-like');
-containerMoreProductsLike.innerHTML = 
-    produto.then((data) => 
-        {renderProdutos(data)}
-    ).catch(error => {
-        console.error('Error rendering more products:', error)
-    });
+const renderProdutos = (lista) => {
+  containerMoreProductsLike.innerHTML = lista
+    .map(
+      (p) => `
+        <div class="produto slide-in" data-id="${p.id}" data-name="${p.nome.split(" ").join("-")}">
+            
+            <div class="produto__imagem">
+                <img src="${p.imagem}" alt="${p.nome}">
+            </div>
+
+            <div class="product--info">
+                <div class="">
+                    <h4>${p.nome}</h4>
+                    <span class="pricetag">${p.preco.toFixed(2)}Kz</span>
+                </div>
+                
+                <div>
+                    <div class="lovebox">
+                        <span class="fas fa-heart like "></span>
+                    </div>
+                    <div class="cart--shopp">
+                        <i class="fa fa-cart-shopping" style="color: grey;"></i>    
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    `,
+    )
+    .join("");
+}
+
+produto.then((data) => {
+  renderProdutos(data);
+});
