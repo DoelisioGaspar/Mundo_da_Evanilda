b2ecf286ba05e364b13b7758c70f0909adf6582b
DoelisioGaspar <doegaspar123@gmail.com>
2026-06-01 15:23:28 +0100

    c--58

--- js/comprar.js
+++ js/comprar.js
@@ -3,9 +3,9 @@ import { produto } from './produtos.js';
 
 export default function renderProductSelected(data) {
     const urlParams = new URLSearchParams(window.location.search);
-    //const productId = urlParams.get('id');
+    const productId = urlParams.get('id');
     const productName = urlParams.get('name').split('-').join(' '); // Substitui hífens por espaços
-    const selectedProduct = data.find(p =>  p.nome === productName);
+    const selectedProduct = data.find(p =>  p.id === productId);
     
     if (selectedProduct) {
         const productDetails = document.querySelector('.container-product');
